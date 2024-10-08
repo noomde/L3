8e01678a492ada57070d9bff62b7b4830def5cc5
ab226rf <ab226rf@student.lnu.se>
2024-10-08 17:27:34 +0200

    added links to sidebar

---
+++ src/js/components/sideBar.js
@@ -0,0 +1,25 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<link rel="stylesheet" href="../../css/app.css">
+  <div class="sidebar">
+    <ul>
+    <li><a href="sum.js"></a></li>
+    <li><a href="sortDescending.js"></a></li>
+    <li><a href="sortAscending.js"></a></li>
+    <li><a href="mean.js"></a></li>
+    <li><a href="geometricMean.js"></a></li>
+    <li><a href="harmonicMean.js"></a></li>
+    <li><a href="median.js"></a></li>
+    <li><a href="convertDecimal.js"></a></li>
+    <li><a href="convertPercentage.js"></a></li>
+    <li><a href="range.js"></a></li>
+    <li><a href="populationVariance.js"></a></li>
+    <li><a href="sampleVariance.js"></a></li>
+    <li><a href="populationStandardDeviation.js"></a></li>
+    <li><a href="sampleStandardDeviation.js"></a></li>
+    <li><a href="mode.js"></a></li>
+    <li><a href="quartiles.js"></a></li>
+    <li><a href="interquartileRange.js"></a></li>
+    </ul>
+  </div>
+`
